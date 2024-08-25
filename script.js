c5e8c75c460cbb258bd3be34f5029bce03499d34
Arthur Granito <arthurcursotecnico@gmail.com>
2024-08-25 20:37:07 -0300

    javascript abrir e fchar menu completo

---
+++ script.js
@@ -0,0 +1,18 @@
+const botaoAbrir = document.getElementById('botao-abrir');
+const botaoFechar = document.getElementById('botao-fechar');
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+
+botaoAbrir.addEventListener('click', abrirMenu);
+
+function abrirMenu() {
+    body.classList.add('escurecer');
+    nav.classList.add('abrir');
+}
+
+botaoFechar.addEventListener('click', fecharMenu);
+
+function fecharMenu() {
+    body.classList.remove('escurecer');
+    nav.classList.remove('abrir');
+}
